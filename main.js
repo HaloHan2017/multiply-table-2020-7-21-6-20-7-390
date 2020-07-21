7cba077a67f5c8ae1f52a5fbbe0b3cf349279f97
HaloHan2017 <1006653740@qq.com>
2020-07-21 20:27:25 +0800

    upload files

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 function createMultiplyTable(start, end) {
+  if(isValid(start , end)){
+    return getPrintStrOfMultiplyTable(start , end);
+  }
   return null;
+}
+
+function isValid(start , end){
+  if(start > end){
+    return false;
+  }
+  if((start < 1 || start > 1000) || (end < 1 || end > 1000)){
+    return false;
+  }
+  return true;
+}
+
+function getPrintStrOfMultiplyTable(start , end){
+  let resultStr = "";
+  for(let i = start; i <= end; i++){
+    for(let j= start; j <= i; j++){
+      resultStr += j + "*" + i + "=" + i * j;
+      if(j < i){
+        resultStr += "\t";
+      }
+    }
+    if(i != end){
+      resultStr += "\n";
+    }
+  }
+  return resultStr;
 }
 
 module.exports = {
